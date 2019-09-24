00f037e7414c29a7daa816b3e00927db3de70009
xuluuy <50956393+xuluuy@users.noreply.github.com>
2019-09-24 15:13:34 +0800

    Update ui.js

--- xiaomiShopping/js/ui.js
+++ xiaomiShopping/js/ui.js
@@ -36,7 +36,6 @@ function TimeDown(id, endDateStr) {
     //当前时间
     var nowDate = new Date();
     //相差的总秒数
-    var totalSecondes = parseInt(endDate - nowDate);
     var totalSeconds = parseInt((endDate - nowDate) / 1000);
     //天数
     // var days = Math.floor(totalSeconds / (60 * 60 * 24));
